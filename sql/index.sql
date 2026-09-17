80076d00d0858c77d7ab1d73c6bbc9770b946cbc
Bharath@90361 <bharath.kumar@bugasura.io>
2026-09-17 10:42:11 +0530

    added sql

---
+++ sql/index.sql
@@ -0,0 +1,107 @@
+create database facebook;
+USE facebook;
+CREATE TABLE tUser (
+    user_id int PRIMARY KEY,
+    Name varchar(50) NOT NULL,
+    Email_id varchar(50) NOT NULL,
+    Password varchar(50) NOT NULL,
+    Address varchar(100),
+    Phone BIGINT(10)
+);
+create table tFreinds(
+     user_id int,
+     friend_id int,
+     foreign key(user_id) references tUser(user_id),
+     foreign key(friend_id) references tUser(user_id)
+ );
+create table tWall(
+     user_id int,
+     posting_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+     post varchar(200) not null,
+     foreign key(user_id) references tUser(user_id)
+ );
+
+--  inserting into tUser
+ insert into tUser values (101,"Bharath","bharath@gmail.com","12345","Bengaluru",9036127399);
+ insert into tUser values (102,"Sharath","sharath@gmail.com","sharath","mysore",8765374234);
+ insert into tUser values (103,"kiran","kiran@gmail.com","k@123","mangalore",6373920473);
+ insert into tUser values (104,"kumar","kumar@gmail.com","123456","mangalore",6373920473);
+ insert into tUser values (105,"harsha","harsha@gmail.com","123456","kolar",9859602996);
+
+-- inserting into tFreinds
+ insert into tFreinds values(101,102);
+ insert into tFreinds values(101,103);
+ insert into tFreinds values(102,101);
+ insert into tFreinds values(102,105);
+ insert into tFreinds values(102,103);
+ insert into tFreinds values(103,105);
+ insert into tFreinds values(104,101);
+ insert into tFreinds values(104,103);
+ insert into tFreinds values(105,102);
+
+--  inserting into tWall
+insert into tWall(user_id,post) values(101,"my post");
+insert into tWall(user_id,post) values(101,"my family");
+insert into tWall(user_id,post) values(102,"trending");
+insert into tWall(user_id,post) values(103,"#first");
+insert into tWall(user_id,post) values(104,"vacation");
+
+--1 Write a query to fetch all information for a person given his name.
+select * from tUser where Name="Bharath";
+
+--2 Write a query to fetch all posts of a person given his name
+select * from tWall where user_id in (select user_id from tUser where Name="Bharath");
+-- or
+SELECT w.* FROM tWall w JOIN tUser u ON w.user_id = u.user_id WHERE u.Name = 'Bharath';
+
+--3 Write a query to fetch all posts of a particular friend of a person, given his name and the friends name.
+SELECT p.*
+FROM tWall p
+JOIN tFriends f
+    ON f.friend_id = p.user_id
+JOIN tUser u
+    ON u.user_id = f.user_id
+WHERE u.Name = 'Sharath'
+AND f.friend_id = (
+      SELECT user_id
+      FROM tUser
+      WHERE Name = 'Bharath'
+);
+--4 Write a query to fetch all friends of a particular friend of a person, given the persons name and friend's name.
+SELECT friend2.*
+FROM tUser person
+JOIN tFriends f1
+    ON person.user_id = f1.user_id
+JOIN tUser friend
+    ON friend.user_id = f1.friend_id
+JOIN tFriends f2
+    ON friend.user_id = f2.user_id
+JOIN tUser friend2
+    ON friend2.user_id = f2.friend_id
+WHERE person.Name = 'Bharath'
+  AND friend.Name = 'Sharath';
+
+
+--5 Write a query to remove a particular friend from a persons list, given the persons name
+DELETE FROM tFriends
+WHERE user_id = (
+    SELECT user_id
+    FROM tUser
+    WHERE Name = 'harsha'
+)
+AND friend_id = (
+    SELECT user_id
+    FROM tUser
+    WHERE Name = 'Sharath'
+);
+
+--6 Write a query to post something on his wall
+
+INSERT INTO tWall (user_id, post)
+VALUES (
+    (SELECT user_id
+     FROM tUser
+     WHERE Name = 'Bharath'),
+    'Hello Bharath!'
+);
+
